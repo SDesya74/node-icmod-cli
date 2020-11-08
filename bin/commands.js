6a8d8a36c82538f66c13b4655bd8a3a399d71639
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2020-11-08 23:53:06 +0300

    Add command registry

--- bin/commands.js
+++ bin/commands.js
@@ -1,33 +1,55 @@
 #!/usr/bin/env node
 
-const { question, selector } = require("./cli");
-const { createProject } = require("./global/project");
+const { question, selectMany, selectOne } = require("./cli");
+const { Project } = require("./project");
+const { join } = require("path");
 
-(async () => {
-	let args = process.argv.slice(2);
-	let command = args[0];
-	switch (command) {
-		case "create":
-			let name = await question("Project name: ");
-			let directory = await question("Project directory name: ", name.replace(/[\u{0080}-\u{FFFF}]/gu, "").replace(" ", "-"));
-			let author = await question("Author: ");
-			let modules = await selector("Check the features needed for your project:", {
-				"Native C++": false,
-				"Java": false,
-				"TypeScript": true
-			});
+const commands = [];
+function registerCommand(regex, action) {
+	commands.push({ regex: regex, action: action });
+}
 
-			await createProject({
-				name: name,
-				directory: directory,
-				author: author,
-				modules: modules
-			}).then(() => {
-				console.log("Project successfully created!");
-				console.log("Use \x1b[33mcd " + directory + "\x1b[0m to start :)");
-			}).catch(reason => console.log("Error: " + reason));
 
+function processCommand(args) {
+	let command = args.join(" ");
+	for (let { regex, action } of commands) {
+		let match = command.match(regex);
+		if (match) {
+			action(match, args);
 			break;
+		}
 	}
-})();
+}
 
+registerCommand(/create/, async match => {
+	let name = await question("Project name: ");
+	let directory = await question("Project directory name: ", name.replace(/[\u{0080}-\u{FFFF}]/gu, "").replace(" ", "-"));
+	let author = await question("Author: ");
+	let modules = await selectMany("Check the features needed for your project:", {
+		"Native C++": false,
+		"Java": false,
+		"TypeScript": true
+	});
+	let ide = await selectOne("Select the IDE you prefer: ", ["VS Code", "PhpStorm"]);
+
+	await Project.create(join(process.cwd(), directory), {
+		name: name,
+		author: author,
+		modules: modules,
+		ide: ide
+	}).then(() => {
+		console.log("Project successfully created!");
+		console.log("Use \x1b[32mcd " + directory + "\x1b[0m to start :)");
+	}).catch(reason => console.log("Error: " + reason));
+});
+
+registerCommand(/build/, async command => {
+	let project = Project.open(process.cwd());
+	if (!project) {
+		console.log("THIS IS NOT ICMOD-PROJECT");
+		return;
+	}
+	project.build();
+});
+
+processCommand(process.argv.slice(2));
