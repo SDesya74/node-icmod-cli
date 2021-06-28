879df58b735c6244013071a8366cf60be83474ed
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-06-28 23:22:47 +0300

    Create commands manager

---
+++ src/util/commands.ts
@@ -0,0 +1,14 @@
+const commands = []
+
+export function registerCommand(regex, action) {
+    commands.push({ regex, action })
+}
+
+export function processCommand(args) {
+    let command = args.join(" ")
+    for (const { regex, action } of commands) {
+        let match = command.match(regex)
+        if (match) return action(match, args)
+    }
+    console.log("Command not found. Type \"icmod help\" to get list of commands.")
+}
