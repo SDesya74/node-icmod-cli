c5fc32b8a26c31f25f5146032de7116b2d22da3c
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-07-01 23:49:06 +0300

    Add help command shortcut and make it more informative

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,11 +1,22 @@
+import { Project } from "../Project"
+
 export default register => {
-    register(/help/, async () => {
-        console.log(`
+    register(/(help|h)/, async () => {
+        try {
+            await Project.find(process.cwd())
+            console.log(`
 Commands:
-  create   Create a project folder
-  help     Displays this message
+  help, -h   Displays this message
+  build, b   Builds project to 'dist' directory
+`)
+        } catch {
+            console.log(`
+Commands:
+  create, c   Create a project folder
+  help, h     Displays this message
   
 Trigger this inside Icmod project (and npm/yarn install) for more commands.
 `)
+        }
     })
 }
