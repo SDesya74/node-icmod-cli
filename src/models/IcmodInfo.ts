a6e5bf4e8a7dbc5d4701e30364f260e17f8fe797
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-07-01 18:57:38 +0300

    Add IcmodInfo and ProjectCompilingSource models

---
+++ src/models/IcmodInfo.ts
@@ -0,0 +1,8 @@
+declare module "models" {
+    export type IcmodInfo = {
+        name: string
+        description: string
+        author: string
+        version: string
+    }
+}
--- src/models/ProjectCompilingSource.ts
+++ src/models/ProjectCompilingSource.ts
@@ -1,6 +1,6 @@
 declare module "models" {
     export type ProjectCompilingSource = {
-        path: "thaumcraft/source"
-        "type": "main" | "launcher" | "preloader" | "native" | "java"
+        path: string
+        type: "main" | "launcher" | "preloader" | "native" | "java"
     }
 }
