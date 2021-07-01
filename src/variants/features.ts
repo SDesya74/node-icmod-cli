610752fab6d90c6f9472b1846f18cc0fa7f98612
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-07-01 18:58:40 +0300

    Add list of available features

---
+++ src/variants/features.ts
@@ -0,0 +1,15 @@
+export default [
+    {
+        label: "TypeScript",
+        value: "typescript",
+        recommended: true
+    },
+    {
+        label: "Native C++",
+        value: "native"
+    },
+    {
+        label: "Java",
+        value: "java"
+    }
+]
