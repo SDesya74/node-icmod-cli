13563c29d6cca20967792114364a0a8c84f3bd4a
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-07-03 23:42:15 +0300

    feat(project): add all config fields

--- src/Project.ts
+++ src/Project.ts
@@ -1,12 +1,41 @@
-import { dirname, join } from "path"
-import { existsSync }    from "fs"
-import { merge } from "./util/merge"
-import { ProjectConfig } from "models"
+import { basename, dirname, join }        from "path"
+import { existsSync }                     from "fs"
+import { merge }                          from "./util/merge"
+import { ProjectConfig }                  from "models"
+import allFeatures                        from "./variants/features"
+import { Icmod }                          from "./Icmod"
+import { info, SpinnerProgress, success } from "./util/cli"
+import { readdir }                        from "fs/promises"
 
 export class Project {
     static configFilename = "icmod.config.js"
+    config: ProjectConfig
+    root: string
+    configFile: string
     
-    private constructor(public config: ProjectConfig) {
+    private constructor(root: string) {
+        this.root = root
+        this.configFile = join(root, Project.configFilename)
+        if (!existsSync(this.configFile)) throw `Config file is not exist in ${ root }`
+    }
+    
+    static async find(dir: string): Promise<Project> {
+        return new Promise(async (resolve, reject) => {
+            while (!existsSync(join(dir, Project.configFilename))) {
+                if (dirname(dir) === dir) return reject()
+                dir = dirname(dir)
+            }
+            
+            const project = new Project(dir)
+            await project.init()
+            resolve(project)
+        })
+    }
+    
+    async init() {
+        const configFunction = require(this.configFile)
+        const loadedConfig = await configFunction()
+        
         this.config = merge({
             info: {
                 version: {}
@@ -14,9 +43,12 @@ export class Project {
             push: {},
             project: {
                 resources: [],
-                compile: []
-            }
-        }, this.config)
+                compile: [],
+                additional: [],
+                features: []
+            },
+            config: {}
+        }, loadedConfig)
     }
     
     async find(dir: string) {
