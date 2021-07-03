387e14663b2bd448f150d06315de2488f898f6ca
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-07-03 23:47:28 +0300

    feat(icmod): make working with files async

--- src/Icmod.ts
+++ src/Icmod.ts
@@ -1,10 +1,10 @@
-import { BuildConfig, IcmodConfig, IcmodInfo }                                     from "models"
-import { closeSync, copyFileSync, existsSync, mkdirSync, openSync, writeFileSync } from "fs"
-import { basename, join }                                                          from "path"
-import { copyFolderRecursive }                                                     from "./util/files"
-import * as AdmZip                                                                 from "adm-zip"
-import { SpinnerProgress }                                                         from "./util/cli"
-import { writeFile }                                                               from "fs/promises"
+import { BuildConfig, IcmodConfig, IcmodInfo } from "models"
+import { closeSync, existsSync, openSync }     from "fs"
+import { basename, join }                      from "path"
+import { copyFolderRecursive }                 from "./util/files"
+import * as AdmZip                             from "adm-zip"
+import { SpinnerProgress }                     from "./util/cli"
+import { copyFile, mkdir, writeFile }          from "fs/promises"
 
 
 export class Icmod {
@@ -37,24 +37,34 @@ export class Icmod {
         }
     }
     
-    build() {
+    async build() {
         if (!this.info) throw `mod info not defined`
         
-        if (!existsSync(this.root)) mkdirSync(this.root, { recursive: true })
+        if (!existsSync(this.root)) await mkdir(this.root, { recursive: true })
         
-        writeFileSync(join(this.root, "mod.info"), JSON.stringify(this.info, null, 4))
-        writeFileSync(join(this.root, "build.config"), JSON.stringify(this.buildConfig, null, 4))
-        writeFileSync(join(this.root, "config.json"), JSON.stringify(this.config, null, 4))
+        await writeFile(join(this.root, "mod.info"), JSON.stringify(this.info, null, 4))
+        await writeFile(join(this.root, "build.config"), JSON.stringify(this.buildConfig, null, 4))
+        await writeFile(join(this.root, "config.json"), JSON.stringify(this.config, null, 4))
         closeSync(openSync(join(this.root, ".nomedia"), "w"))
         
-        if (this.iconFile) copyFileSync(this.iconFile, join(this.root, "mod_icon.png"))
+        if (this.iconFile) await copyFile(this.iconFile, join(this.root, "mod_icon.png"))
     }
     
-    addResourceDirectory(sourcePath: string, type: "gui" | "resources" | "resource_pack" | "behavior_pack") {
+    async zipTo(file: string) {
+        const zip = new AdmZip()
+        zip.addLocalFolder(this.root, basename(this.root))
+        await writeFile(file, zip.toBuffer())
+    }
+    
+    async addResourceDirectory(sourcePath: string, type: string, progress?: SpinnerProgress) {
         const { path, resourceType = null } = Icmod.resourceTranslator[type]
+        const dirname = basename(sourcePath)
         
-        copyFolderRecursiveSync(sourcePath, join(this.root, path))
+        const targetPath = join(this.root, path)
+        progress?.setLabel(`Building ${ type } ${ dirname !== type ? `(${ dirname })` : "" }`)
         
-        if (resourceType) this.config.resources.push({ path, resourceType })
+        await copyFolderRecursive(sourcePath, targetPath)
+        
+        if (resourceType) this.buildConfig.resources.push({ path, resourceType })
     }
 }
