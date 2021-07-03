7746a4d9f00120620209742242b55150cd21e11d
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-07-03 23:36:49 +0300

    feat(models): add config and features to ProjectConfig

--- src/models/ProjectConfig.ts
+++ src/models/ProjectConfig.ts
@@ -1,25 +1,31 @@
 declare module "models" {
-     export type ProjectConfig = {
-         info: {
-             name: string
-             description: string
-             author: string
-             version: {
-                 name: string
-                 code: number
-             }
-         }
-         
-         push: {
-             ip: string
-             port: number
-             modsDirectory: string
-         }
-         
-         project: {
-             resources: ProjectResource[]
-             compile: ProjectCompilingSource[]
-             additional: ProjectAdditionalComponent[]
-         }
-     }
+    export type ProjectConfig = {
+        info: {
+            name: string
+            description: string
+            author: string
+            icon: string
+            version: {
+                name: string
+                code: number
+            }
+        }
+        
+        push: {
+            ip: string
+            port: number
+            modsDirectory: string
+        }
+        
+        project: {
+            resources: ProjectResource[]
+            features: string[]
+            compile: ProjectCompilingSource[]
+            additional: ProjectAdditionalComponent[]
+        }
+        
+        config: {
+            [key: string]: any
+        }
+    }
 }
