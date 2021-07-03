f19a3596df2988ec7ce629547968409ec292fea9
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-07-03 23:38:14 +0300

    feat(cli): add some decorative methods to print text in console

--- src/util/cli.ts
+++ src/util/cli.ts
@@ -5,12 +5,25 @@ async function readKey(): Promise<Key> {
     return new Promise(async resolve => process.stdin.once("keypress", (_, key) => resolve(key)))
 }
 
-function decoratePrompt(text) {
-    return ` \x1b[32m?\x1b[0m ${ text }`
+function ask(text) {
+    console.log(` \x1b[32m?\x1b[0m ${ text }`)
 }
 
 function liftUpConsoleCursor(lineAmount) {
     console.log(`\x1b[${ lineAmount + 1 }A`)
+}
+// endregion
+
+export function info(...data: any[]) {
+    console.log(` \x1b[32mi\x1b[0m`, ...data)
+}
+
+export function warn(...data: any[]) {
+    console.warn(` \x1b[32m!\x1b[33m`, ...data, `\x1b[0m`)
+}
+
+export function success(...data: any[]) {
+    console.log(`\x1b[32m`, ...data, `\x1b[0m`)
 }
 
 export async function question(prompt, defaultValue = null): Promise<string> {
@@ -24,7 +37,7 @@ export async function question(prompt, defaultValue = null): Promise<string> {
                 })
                 
                 reader.question(
-                    decoratePrompt(`${ prompt }${ defaultValue ? "(" + defaultValue + ") " : "" }`),
+                    ` \x1b[32m?\x1b[0m ${ prompt }${ defaultValue ? "(" + defaultValue + ") " : "" }`,
                     answer => {
                         resolve(answer)
                         reader.close()
@@ -60,7 +73,7 @@ export async function selectMany(prompt, items): Promise<string[]> {
         }).join(EOL))
     }
     
-    console.log(decoratePrompt(prompt))
+    ask(prompt)
     showCurrentState()
     
     return new Promise(async (resolve) => {
@@ -106,7 +119,7 @@ export async function selectOne<T>(prompt, items: SelectOneOption<T>[]): Promise
         )
     }
     
-    console.log(decoratePrompt(prompt))
+    ask(prompt)
     showCurrentState()
     
     return new Promise(async resolve => {
@@ -124,6 +137,39 @@ export async function selectOne<T>(prompt, items: SelectOneOption<T>[]): Promise
         
         resolve(items[selection].value)
     })
+}
+
+export class SpinnerProgress {
+    charList = "⠈⠐⠠⢀⡀⠄⠂⠁"
+    private index: number = 0
+    private timeout: Timeout
+    private label: string
+    
+    show() {
+        if (this.timeout) return
+        this.timeout = setInterval(() => {
+            this.index++
+            this.showCurrentState()
+        }, 60)
+        this.showCurrentState()
+    }
+    
+    setLabel(label: string) {
+        this.label = label
+    }
+    
+    stop() {
+        clearInterval(this.timeout)
+        console.log(" ".repeat(50))
+    }
+    
+    private showCurrentState() {
+        if (!this.timeout) throw "Call .show() before showing state"
+        
+        const char = this.charList[this.index % this.charList.length]
+        console.log(` ${ char } ${ this.label }${ " ".repeat(50) }`)
+        liftUpConsoleCursor(1)
+    }
 }
 
 // region KeyReader
