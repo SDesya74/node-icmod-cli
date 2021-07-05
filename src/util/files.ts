f058d890710dae30285d8d21c9ad9e77050bf746
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-07-05 23:58:09 +0300

    refactor(files): make code look prettier

--- src/util/files.ts
+++ src/util/files.ts
@@ -3,24 +3,16 @@ import { basename, join }                      from "path"
 import { mkdir, readdir, writeFile }           from "fs/promises"
 
 async function copyFile(source, target) {
-    let targetFile = target
-    if (existsSync(target)) {
-        if (lstatSync(target).isDirectory()) {
-            targetFile = join(target, basename(source))
-        }
-    }
-    
+    const targetFile = existsSync(target) && lstatSync(target).isDirectory() ? join(target, basename(source)) : target
     await writeFile(targetFile, readFileSync(source))
 }
 
 export async function copyFolderRecursive(source, target, targetDirName = null) {
-    let files = []
-    
     const targetFolder = targetDirName ?? join(target, basename(source))
     if (!existsSync(targetFolder)) await mkdir(targetFolder, { recursive: true })
     
     if (lstatSync(source).isDirectory()) {
-        files = await readdir(source)
+        const files = await readdir(source)
         for (const file of files) {
             const curSource = join(source, file)
             if (lstatSync(curSource).isDirectory()) {
